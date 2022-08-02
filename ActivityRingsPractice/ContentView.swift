f56494df0cfb793f769c576439624f1c6b7cc49f
EhsanTgv <EhsanTgv@gmail.com>
2022-08-02 10:34:36 +0300

    Add Other Rings

--- ActivityRingsPractice/ContentView.swift
+++ ActivityRingsPractice/ContentView.swift
@@ -9,18 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     @State var percentage1: Double = 0
-    
+    @State var percentage2: Double = 0
+    @State var percentage3: Double = 0
+
     var body: some View {
         ZStack{
             Ring(
-                lineWidth: 40,
+                lineWidth: 30,
                 backgroundColor: Color.blue.opacity(0.2),
                 foregroundColor: Color.blue,
                 percentage: percentage1
             )
+            .frame(width: 140, height: 140)
+            .onTapGesture {
+                self.percentage1 = 70
+            }
+            Ring(
+                lineWidth: 40,
+                backgroundColor: Color.yellow.opacity(0.2),
+                foregroundColor: Color.yellow,
+                percentage: percentage2
+            )
+            .frame(width: 220, height: 220)
+            .onTapGesture {
+                self.percentage2 = 60
+            }
+            Ring(
+                lineWidth: 40,
+                backgroundColor: Color.red.opacity(0.2),
+                foregroundColor: Color.red,
+                percentage: percentage3
+            )
             .frame(width: 300, height: 300)
             .onTapGesture {
-                self.percentage1 = 100
+                self.percentage3 = 80
             }
         }
     }
