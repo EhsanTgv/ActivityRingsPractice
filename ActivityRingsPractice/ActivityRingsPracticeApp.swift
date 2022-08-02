328d2b7b5232168ca04dc5a7166016d18a2fdc79
EhsanTgv <EhsanTgv@gmail.com>
2022-08-02 09:54:08 +0300

    Initial Commit

---
+++ ActivityRingsPractice/ActivityRingsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingsPracticeApp.swift
+//  ActivityRingsPractice
+//
+//  Created by Ehsan Taghavi on 2.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
